8ab83fb9c66a0cb8f37c4639d212e82669eabcd2
Clint Jones <clinton.alan.jones@gmail.com>
2025-01-30 11:29:48 -0800

    Added client authentication

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,88 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
 
+// Ensure the shape of decoded the auth token for type safety
+interface DecodedToken extends JwtPayload {
+  username: string;
+  iat: number; // Issued At Time (Unix format, e.g. 1738261964)
+  exp: number; // Expiration (Unix format, e.g. 1738265564)
+}
+
 class AuthService {
-  getProfile() {
-    // TODO: return the decoded token
+
+  // return the decoded token
+  getProfile(): DecodedToken | null {
+    const token = this.getToken();
+    if (token) {
+      try {
+        const decodedToken = jwtDecode<DecodedToken>(token);
+
+        return decodedToken;
+      } catch (error) {
+        console.error('Error decoding the token:', error);
+      }
+    }
+    return null;
   }
 
-  loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
-  }
-  
-  isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+  // Check if the user is logged in 
+  // The user is only logged in if we can successfully retrieve the token from 
+  // localStorage and the token's exp claim (expiration date) has not expired
+  loggedIn(): boolean {
+    const token = this.getToken();  // read from localStorage
+
+    if (token) {
+      // the user has a token
+      const decodedToken = this.getProfile();
+
+      if (decodedToken) {
+        // return true only if the token has not expired, false otherwise
+        return !this.isTokenExpired(decodedToken);
+      }
+    }
+
+    return false;
   }
 
+  // Return true if the token is expired, false otherwise
+  isTokenExpired(decodedToken: DecodedToken): boolean {
+    // Convert the payload's 'exp' timestamp value from seconds to milliseconds
+    const expDate = decodedToken.exp * 1000;
+    // Get current time in milliseconds
+    const currentDate = new Date().getTime();
+
+    // Check if the token is expired by comparing the expiration time with the current time
+    if (expDate < currentDate) {
+      console.log(`Login session expired.`);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  // Retrieve the encoded JWT token from localStorage
   getToken(): string {
-    // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    
+    return loggedUser;
   }
 
+  // Store the JWT token in localStorage then redirect to the home page
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    // set the token to localStorage
+    localStorage.setItem('id_token', idToken);
+
+    // redirect to the home page
+    window.location.assign('/');
   }
 
+  // Remove the JWT token from localStorage then redirect to the home page
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    // remove the token from localStorage
+    localStorage.removeItem('id_token');
+
+    // redirect to the login page
+    window.location.assign('/');
   }
 }
 
